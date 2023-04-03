3bd7cd604d9dc113fdeb12c28e7ef813417b28a7
Koussay Dhifi <koussaydhifi48@gmail.com>
2023-04-03 16:02:07 +0100

    Finish About page

--- views/views.py
+++ views/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint,render_template
 
-view = Blueprint(__name__,template_folder=template_view,static_folder=static_view)
+view = Blueprint('view',__name__,template_folder='template_view',static_folder='static_view')
 
 
-@view.route("/upload")
-def upload():
-    return render_template("upload.html")
+@view.route("/about")
+def about():
+    return render_template("about.html")
+
