dbedf2df95a0148968e866421aa99e45b2b577dd
Koussay Dhifi <koussaydhifi48@gmail.com>
2023-04-03 17:42:08 +0100

    Add Login page

--- views/views.py
+++ views/views.py
@@ -7,3 +7,7 @@ view = Blueprint('view',__name__,template_folder='template_view',static_folder='
 def about():
     return render_template("about.html")
 
+@view.route("/login",methods=["GET","POST"])
+def login():
+    return render_template("login.html")
+
